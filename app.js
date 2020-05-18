c96e6f2fa74a6c8e0a3d50cc1a95ad03066ac70a
Keefer Gregoire <keefergregoire@keefers-mbp.lan>
2020-05-18 14:46:54 -0400

    hope this works

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public')); //for js scripts
+app.use(express.static(__dirname + '/Views')); //for js scripts
 app.use(express.static('client/build'));
 
 app.get('/', function(req, res){
