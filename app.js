8b66bc8c6751f59f9ec258083d630acd1acf4b3f
Keefer Gregoire <keefergregoire@keefers-mbp.lan>
2020-05-18 14:21:47 -0400

    file moved home route to app

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express 			= require("express"),
     bodyParser			= require('body-parser'),
     mongoose 			= require('mongoose');
 
-var indexRoutes 		= require('./routes/index');
+// var indexRoutes 		= require('./⁨routes⁩/index');
 
 
 
@@ -13,7 +13,9 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public')); //for js scripts
 app.use(express.static('client/build'));
 
-app.use(indexRoutes);
+app.get('/', function(req, res){
+		res.render('home');
+});
 
 let port = process.env.PORT;
 if (port == null || port == "") {
